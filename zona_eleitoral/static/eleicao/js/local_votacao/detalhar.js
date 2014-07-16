a30e5cfa74b366909293fe2e91b9829b89366ffd
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2014-07-15 22:05:25 -0300

    a

--- zona_eleitoral/static/eleicao/js/local_votacao/detalhar.js
+++ zona_eleitoral/static/eleicao/js/local_votacao/detalhar.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	
 	$('.bt-desacoplar').click(function(event){
 		$(this).confirmar({
-			message: "Você deseja desacoplar estas seções?",
+			message: "Você deseja desagregar estas seções?",
 			onConfirm: function() {
 				secao.setIdSecao($(this).data('secao'));
 				secao.desagregarSecao();
@@ -22,8 +22,12 @@ $(document).ready(function(){
 	});
 	
 	$('#bt-agregar').click(function(event){
+		if($('[name=pk_secao]:checked').length<2){
+			$.notify({title:'Selecione pelo menos duas seções'});
+			return;
+		}
 		$(this).confirmar({
-			message: "Você deseja excluir esta seção?",
+			message: "Você deseja agregar esta seção?",
 			onConfirm: function() {
 				secao.agregarSecoes();
 			}
