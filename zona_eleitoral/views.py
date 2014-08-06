e2451ee13ea54c9830fffa112b8245436620dfb9
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2014-08-05 21:14:27 -0300

    a

--- zona_eleitoral/views.py
+++ zona_eleitoral/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from acesso.models import OrgaoPublico
 # Create your views here.
 
 @login_required
 def home(request):
-    if request.user.is_orgao():
+    if isinstance(request.user, OrgaoPublico):
         return redirect('veiculos:index')
     titulo = u'Seja Bem Vindo'
     return render(request, 'home.html', locals())
