c7252cea96bacd042269d9faa9905cd5135e276b
Thyago Neves <thyagomn@gmail.com>
2014-10-01 23:36:31 -0300

    Ajustes no arquivo de produção

--- zona_eleitoral/settings/production.py
+++ zona_eleitoral/settings/production.py
@@ -6,10 +6,41 @@ MY_APPS = (
     'acesso',
     'veiculos',
     'django_extensions',
-    'south'
+    'south',
+    'webodt',
+    'jquery',
+    'staticfiles_select2',
+    'selectable',
+    'selectable_select2',
+    'dajaxice',
+    'dajax',
 )
 
 INSTALLED_APPS = INSTALLED_APPS + MY_APPS
+
+WEBODT_CONVERTER = 'webodt.converters.abiword.AbiwordODFConverter'
+WEBODT_ODF_TEMPLATE_PREPROCESSORS = (
+    'webodt.preprocessors.xmlfor_preprocessor',
+    'webodt.preprocessors.unescape_templatetags_preprocessor'
+)
+
+WEBODT_TEMPLATE_PATH = LOCAL('odt_templates')
+WEBODT_GOOGLEDOCS_EMAIL = 'usuariozon029@gmail.com'
+WEBODT_GOOGLEDOCS_PASSWORD = 'Zon@0029'
+
+SELECTABLE_ESCAPED_KEYS = ('label', 'value')
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'dajaxice.finders.DajaxiceFinder',
+)
 
 #AUTH_USER_MODEL = 'acesso.Usuario'
 
