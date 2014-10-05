2c83f5180164523debc5e6dff5109fac10ef4669
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2014-10-05 14:54:18 -0300

    Relatorio de veiculos alocados

--- apuracao/models.py
+++ apuracao/models.py
@@ -24,9 +24,14 @@ class Apuracao(models.Model):
 def importar_dados():
     importacao = get_dados(settings.APURACAO_XML_LOCAIS, settings.APURACAO_XML_ABRANGENCIA, settings.APURACAO_PATH)
     for dicionario in importacao:
-        cidade = Cidade.objects.get_or_create(codigo=dicionario['codigo'], nome=dicionario['cidade'], uf=dicionario['UF'], secoes=dicionario['secoes'])[0]
-        print cidade
-        apuracao = Apuracao.objects.get_or_create(cidade=cidade,
+        if Cidade.objects.filter(codigo=dicionario['codigo']).exists():
+            cidade = Cidade.objects.get(codigo=dicionario['codigo'])
+        else:
+            cidade = Cidade.objects.create(codigo=dicionario['codigo'], nome=dicionario['cidade'], uf=dicionario['UF'], secoes=dicionario['secoes'])
+            print cidade
+        if Apuracao.objects.filter(cidade=cidade, dt_atualizacao=dicionario['dt_atualizacao']).exists():
+            continue
+        apuracao = Apuracao.objects.create(cidade=cidade,
                                                   dt_atualizacao=dicionario['dt_atualizacao'],
                                                     secoes_totalizadas=dicionario['secoes_totalizadas'],
                                                      secoes_restantes=dicionario['secoes_restantes'],
